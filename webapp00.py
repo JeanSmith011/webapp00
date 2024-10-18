c8d73f407d3b335c4cdae49961b12ebe408d8cd8
JeanSmith011 <jsnegocioonline@gmail.com>
2024-10-18 20:32:51 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -7,16 +7,16 @@ db.fillna('', inplace=True)
 Escrever(db)
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
+st.title("Bom de Papo")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Hejheheheh! Prof. Massaki")
+st.header("Internet dms")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Rapido")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("CO Melhor ta demais!")
 
 values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
 st.write("Values:", values)
